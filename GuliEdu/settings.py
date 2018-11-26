f099274b5eb9dc6ae92c544429c84fe71340dff0
wangzx <wangzxcoco@sina.com>
2018-11-26 18:40:15 +0800

    配置setting

--- GuliEdu/settings.py
+++ GuliEdu/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'GuliEdu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'guliedu',
+        'USER': 'root',
+        'PASSWORD': 'xiao123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -105,15 +109,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
