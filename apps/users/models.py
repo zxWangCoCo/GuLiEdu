ef4959476d53bab10cbe92fb388daed897f5b321
wangzx <wangzxcoco@sina.com>
2018-11-27 20:26:21 +0800

    创建用户表

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from datetime import datetime
 
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class UserProfile(AbstractUser) :
+    #头像
+    image = models.ImageField(upload_to='user/',max_length=200,verbose_name="用户头像",null=True,blank=True)
+    #昵称
+    nick_name = models.CharField(max_length=20,verbose_name="用户昵称",null=True,blank=True)
+    #生日
+    birthday = models.DateTimeField(verbose_name="用户生日",null=True,blank=True)
+    #性别
+    gender = models.CharField(choices=(('girl','女'),('boy','男')),max_length=10,verbose_name="用户性别",default='gril')
+    #会员地址
+    address = models.CharField(max_length=20,verbose_name="地址",null=True,blank=True)
+    #会奥运手机
+    phone = models.CharField(max_length=11,verbose_name="用户手机",null=True,blank=True)
+    #创建时间
+    add_time = models.DateTimeField(default=datetime.now,verbose_name="创建时间")
+
+    def __str__(self):
+        return self.username
+
+    class Meta :
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
