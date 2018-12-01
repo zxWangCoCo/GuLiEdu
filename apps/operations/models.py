60e8838cd92b710962bd658705805e9695f13f7d
wangzx <wangzxcoco@sina.com>
2018-12-01 16:18:27 +0800

    增加操作相关表结构

--- apps/operations/models.py
+++ apps/operations/models.py
@@ -1,3 +1,75 @@
+from datetime import datetime
+
 from django.db import models
 
+from users.models import UserProfile
+from courses.models import CourseInfo
+
 # Create your models here.
+
+class UserAsk(models.Model):
+    name = models.CharField(verbose_name="姓名",max_length=30)
+    phone = models.CharField(verbose_name="手机号",max_length=11)
+    course = models.CharField(verbose_name="课程",max_length=20)
+    add_time = models.DateTimeField(default=datetime.now,verbose_name="创建时间")
+
+    def __str__(self):
+        return  self.name
+
+    class Meta :
+        verbose_name = "咨询信息"
+        verbose_name_plural = verbose_name
+
+class UserLove(models.Model):
+    love_man = models.ForeignKey(UserProfile,verbose_name="收藏用户")
+    love_id = models.IntegerField(verbose_name="收藏的id")
+    love_type = models.IntegerField(choices=((1,'org'),(2,'course'),(3,'teacher')),verbose_name="收藏类别")
+    love_status = models.BooleanField(default=False,verbose_name="收藏状态")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="收藏时间")
+
+    def __str__(self):
+        return  self.love_man.username
+
+    class Meta :
+        verbose_name = "收藏信息"
+        verbose_name_plural = verbose_name
+
+class UserCourse(models.Model):
+    study_man = models.ForeignKey(UserProfile,verbose_name="学习用户")
+    study_course = models.ForeignKey(CourseInfo,verbose_name="学习课程")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="收藏时间")
+
+    def __str__(self):
+        return  self.study_man.username
+
+    class Meta :
+        unique_together = ('study_man','study_course')#去重，再次学习这个课程不添加数据
+        verbose_name = "用户学习课程信息"
+        verbose_name_plural = verbose_name
+
+class UserComment(models.Model):
+    comment_man = models.ForeignKey(UserProfile, verbose_name="评论用户")
+    comment_course = models.ForeignKey(CourseInfo, verbose_name="评论课程")
+    comment_content = models.CharField(verbose_name="评论内容",max_length=300)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="评论时间")
+
+    def __str__(self):
+        return self.comment_man.username
+
+    class Meta:
+        verbose_name = "用户评论课程信息"
+        verbose_name_plural = verbose_name
+
+class UserMessage(models.Model):
+    message_man = models.IntegerField(default=0,verbose_name="消息用户")#0=系统消息；
+    message_content = models.CharField(verbose_name="消息内容",max_length=200)
+    message_status = models.BooleanField(default=False,verbose_name="消息状态")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="消息时间")
+
+    def __str__(self):
+        return self.message_content
+
+    class Meta:
+        verbose_name = "用户消息"
+        verbose_name_plural = verbose_name
+
