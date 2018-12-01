d7d6112d01f068f1d62f79e55da5ed1701ef51dd
wangzx <wangzxcoco@sina.com>
2018-12-01 17:57:44 +0800

    修改路由bug，并添加app_name,防止报错

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -13,10 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url ,include
-from django.contrib import admin
-
+from django.conf.urls import url
+app_name = 'users'
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^users/',include('users.urls',namespace='users'))
 ]
