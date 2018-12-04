cffc13ef17fee30cd63eba1418cc23527b365dd2
wangzx <wangzxcoco@sina.com>
2018-12-04 20:17:34 +0800

    配置xadmin后台模块 机构模块

---
+++ apps/orgs/adminx.py
@@ -0,0 +1,34 @@
+import xadmin
+from .models import CityInfo,OrgInfo,TeacherIfo
+################################城市###################################################
+class CityInfoXadmin(object):
+    list_display = ['name','add_time']
+
+    # xadmin增加搜索框
+    search_fields = ['name']
+
+    # xadmin增加过滤器
+    list_filter = ['name','add_time']
+
+##############################机构#####################################################
+class OrgInfoXadmin(object):
+    list_display = ['image','name','course_num','course_num','study_num','love_num','click_num','category','cityinfo']
+
+    # xadmin增加搜索框
+    search_fields = ['image','name','course_num','course_num','study_num','love_num','click_num','category','cityinfo']
+
+    # xadmin增加过滤器
+    list_filter = ['image','name','course_num','course_num','study_num','love_num','click_num','category','cityinfo']
+
+#####################################教师表############################################
+class TeacherIfoXadmin(object):
+    list_display = ['image', 'name', 'work_year', 'work_position','age', 'gender', 'love_num']
+
+    search_fields = ['image', 'name', 'work_year', 'work_position', 'age', 'gender', 'love_num']
+
+    list_filter = ['image', 'name', 'work_year', 'work_position', 'age', 'gender', 'love_num']
+
+xadmin.site.register(CityInfo,CityInfoXadmin)
+xadmin.site.register(OrgInfo,OrgInfoXadmin)
+xadmin.site.register(TeacherIfo,TeacherIfoXadmin)
+
