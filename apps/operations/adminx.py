6752b6c54e7685c716da81f14f148f5c15cdeff6
wangzx <wangzxcoco@sina.com>
2018-12-04 20:16:51 +0800

    配置xadmin后台模块 机构模块

---
+++ apps/operations/adminx.py
@@ -0,0 +1,46 @@
+import xadmin
+from .models import UserAsk,UserLove,UserCourse,UserComment,UserMessage
+class UserAskXadmin(object):
+    list_display = ['name', 'phone','course','add_time']
+
+    search_fields = ['name', 'phone', 'course']
+
+    list_filter = ['name', 'phone', 'course', 'add_time']
+
+class UserLoveXadmin(object):
+    list_display = ['love_man', 'love_id', 'love_type', 'love_status','add_time']
+
+    search_fields = ['love_man', 'love_id', 'love_type', 'love_status', 'add_time']
+
+    list_filter = ['love_man', 'love_id', 'love_type', 'love_status', 'add_time']
+
+
+class UserCourseXadmin(object):
+    list_display = ['study_man', 'study_course','add_time']
+
+    search_fields = ['study_man', 'study_course']
+
+    list_filter = ['study_man', 'study_course']
+
+
+class UserCommentXadmin(object):
+
+    list_display = ['comment_man', 'comment_course', 'comment_content','add_time']
+
+    search_fields = ['comment_man', 'comment_course', 'comment_content']
+
+    list_filter = ['comment_man', 'comment_course', 'comment_content']
+
+class UserMessageXadmin(object):
+
+    list_display = ['message_man', 'message_content', 'message_status', 'add_time']
+
+    search_fields = ['message_man', 'message_content', 'message_status']
+
+    list_filter = ['message_man', 'message_content', 'message_status']
+
+xadmin.site.register(UserAsk,UserAskXadmin)
+xadmin.site.register(UserLove,UserLoveXadmin)
+xadmin.site.register(UserCourse,UserCourseXadmin)
+xadmin.site.register(UserComment,UserCommentXadmin)
+xadmin.site.register(UserMessage,UserMessageXadmin)
