c86d1f47ff18180c4a94ea5cd6a8467d69aebb62
wangzx <wangzxcoco@sina.com>
2018-12-07 18:16:36 +0800

    设置后台管理界面显示

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -10,19 +10,24 @@ class BannerInfoXadmin(object) :
 
     #xadmin增加过滤器
     list_filter = ['image','url','add_time']
+
+    model_icon = 'fa fa-user'
 ########################################################################################################################
 #邮箱验证
 class EmailVerifyCodeXadmin(object) :
     list_display = ['code', 'email', 'send_type','add_time']
 
+    model_icon = 'fa fa-user'
+
+#配置胡天管理系统开关开启
 class BaseSetting(object):
     enable_themes=True
-    use_bootswatch=True
+    use_bootswatch=True #启用bootstarp自己的主题
 
 class GlobalSettings(object):
-    site_title="后台管理系统" #更改后天名称
-    site_footer="青岛联众科技有限公司"
-    menu_style="accordion"
+    site_title="后台管理系统" #更改后台名称
+    site_footer="青岛联众科技有限公司"#更改后台尾部名称
+    menu_style="accordion"#设置折叠效果
 
 #注册到xadmin
 xadmin.site.register(BannerInfo,BannerInfoXadmin)
