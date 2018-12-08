6f75b3e3675d14e0d62ec66f2d4adb7550fe4aff
wangzx <wangzxcoco@sina.com>
2018-12-08 18:12:40 +0800

    增加注册跳转

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from .views import user_register
 app_name = 'users'
 urlpatterns = [
+    url(r'^user_register/$',user_register,name="user_register")
 ]
