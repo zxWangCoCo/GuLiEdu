569f5d0a84808a45a706bbcb3ce881200721286d
wangzx <wangzxcoco@sina.com>
2018-12-08 18:13:00 +0800

    增加注册跳转

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from .forms import UserRegisterForm
 # Create your views here.
-
+#跳转首页
 def index(request):
     return render(request,'index.html')
+
+def user_register(request):
+    if request.method == 'GET':#get请求是跳页面
+        return render(request,'register.html')
+    else:#post请求提交
+        user_register_form = UserRegisterForm(request.POST)
+        if user_register_form.is_valid():
+            email = user_register_form.changed_data['email']
+            password = user_register_form.changed_data['password']
