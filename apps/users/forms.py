cea327bf07383ce2131aeecc045a18c371886014
wangzx <wangzxcoco@sina.com>
2018-12-08 18:12:15 +0800

    添加注册表单

---
+++ apps/users/forms.py
@@ -0,0 +1,8 @@
+
+from django import forms
+
+class UserRegisterForm(forms.Form):
+    #验证邮箱
+    email = forms.EmailField(required=True)
+    #密码
+    password = forms.CharField(required=True,min_length=3,max_length=15,error_messages={'required':'密码必须填写','min_length':'密码必须大于3位','max_length':'密码最长为15为'})
