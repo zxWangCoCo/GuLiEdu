0d9f79ff27838454660a9cf8a6e88881da06d60f
wangzx <wangzxcoco@sina.com>
2019-01-01 15:55:02 +0800

    增加会员登录退出逻辑

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,12 +1,14 @@
 from django.db.models import Q
 from django.shortcuts import render,redirect,reverse
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserLoginForm
 from .models import UserProfile
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 #跳转首页
 def index(request):
     return render(request,'index.html')
 
+############################用户注册##############################################################
 def user_register(request):
     if request.method == 'GET':#get请求是跳页面
         return render(request,'register.html')
@@ -28,3 +30,31 @@ def user_register(request):
                 return redirect(reverse('index'))
         else:
             return render(request, 'register.html', {'user_register_form': user_register_form })
+############################用户注册##############################################################
+
+############################用户登录##############################################################
+def user_login(request):
+    if request.method == 'GET':
+        return render(request,'login.html')
+    else:
+        user_login_form = UserLoginForm(request.POST)
+        if user_login_form.is_valid():
+            email = user_login_form.cleaned_data['email']
+            password = user_login_form.cleaned_data['password']
+
+            #查询是否有
+            user = authenticate(username=email,password=password)
+            if user:
+                login(request,user)
+                return redirect(reverse('index'))
+            else:
+                return render(request,'login.html',{
+                    'msg':'邮箱或者密码有误'
+                })
+        else:
+            return render(request, 'login.html', {'user_login_form': user_login_form})
+
+##################################退出############################################################
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('index'))
