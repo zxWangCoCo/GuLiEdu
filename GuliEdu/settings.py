edee81bd7ae5e232989103bdbf575547ad3225e2
wangzx <wangzxcoco@sina.com>
2019-06-04 15:56:00 +0800

    增加日志打印

--- GuliEdu/settings.py
+++ GuliEdu/settings.py
@@ -153,3 +153,33 @@ EMAIL_HOST_USER = 'zhangsanfeng256'
 #密码
 EMAIL_HOST_PASSWORD = 'zhangsanfeng789'
 EMAIL_FROM = 'zhangsanfeng256@163.com'
+
+# 打印日志
+LOGGING = {
+    'disable_existing_loggers': False,
+    'version': 1,
+    'handlers': {
+        'console': {
+            # logging handler that outputs log messages to terminal
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG', # message level to be written to console
+        },
+    },
+    'loggers': {
+        '': {
+            # this sets root level logger to log debug and higher level
+            # logs to console. All other loggers inherit settings from
+            # root level logger.
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False, # this tells logger to send logging message
+                                # to its parent (will send if set to True)
+        },
+        'django.db': {
+            # # django also has database level logging
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
