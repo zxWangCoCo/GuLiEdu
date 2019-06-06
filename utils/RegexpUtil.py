af2340e6ad0c86a637f079141f63959f2fe2a701
wangzx <wangzxcoco@sina.com>
2019-06-06 09:54:40 +0800

    验证邮箱

--- utils/RegexpUtil.py
+++ utils/RegexpUtil.py
@@ -8,10 +8,20 @@ class RegexpUtil:
     '''
     REGEX_MOBILE = r'^1[3-9]\d{9}$'
 
+    '''
+        验证邮箱
+    '''
+    REGEX_EMAIL = r'^\w+@(\w+\.)+(com|cn|net|edu)$'
+
     @staticmethod
     def check_phone(*, phone):
         return re.compile(RegexpUtil.REGEX_MOBILE).match(phone)
 
+    @staticmethod
+    def check_email(*, email):
+        return re.compile(RegexpUtil.REGEX_EMAIL).match(email)
+
 
 if __name__ == '__main__':
-    print(RegexpUtil.check_phone(phone='15866522833'))
+    print('phone', RegexpUtil.check_phone(phone='15866522833'))
+    print('email', RegexpUtil.check_email(email='443269741@qq.com'))
