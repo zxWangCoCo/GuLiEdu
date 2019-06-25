c0009c9b592c9289f979b71f79cfb0aa7e39ec8d
wangzx <wangzxcoco@sina.com>
2019-06-25 19:18:33 +0800

    完成学习了该课程的人还学习了什么

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import CourseInfo
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from operations.models import UserLove
+from operations.models import UserLove,UserCourse
 # Create your views here.
 
 def course_list(request):
@@ -55,8 +55,28 @@ def course_detail(request,id):
 def course_video(request,course_id):
     if course_id:
         course = CourseInfo.objects.filter(id=int(course_id))[0]
+        #当用户点击学习时,代表该用户学习了该课程，如果没有学习则加上学习操作
+        usercourse_list = UserCourse.objects.filter(study_course=course,study_man=request.user)
+        if not usercourse_list:
+            a = UserCourse()
+            a.study_man = request.user
+            a.study_course = course
+            a.save()
+
+        #学过该课程的学生还学过什么
+        usercourse_list = UserCourse.objects.filter(study_course=course)
+        #列表生成式
+        user_list = [usercourse.study_man for usercourse in usercourse_list]
+        #__in:在哪个范围内
+        # exclude去除...
+        usercourse_list = UserCourse.objects.filter(study_man__in=user_list).exclude(study_course=course)
+        # 拿到课程
+        # set()用于去重
+        course_list = list(set([usercourse.study_course for usercourse in usercourse_list]))
+
         return render(request,'courses/course-video.html',{
-            'course':course
+            'course':course,
+            'course_list':course_list
         })
 
 
